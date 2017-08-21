20d65be1be1d57eea8b49a6ae7b6e8f821561cca
Djaafar Boumaza <26055564+djaafarcode@users.noreply.github.com>
2017-08-21 22:12:54 +0200

    edit bot.js and change theem index.html

--- bot/bot.js
+++ bot/bot.js
@@ -1,7 +1,18 @@
 /*
 Function control bot.
-doc : http://expressjs.com/en/starter/basic-routing.html
+doc : http://expressjs.com/fr/starter/basic-routing.html
 */
+
+//ROUTAGE AVEC L'INSTANCE EXPRESS DU DERIVER DE HTTP.
+
+let express = require('express'); //renvoie une object.
+let myAppBot = express(); //Crée une app express. 
+//utilise l'object et sa function static qui permet
+myAppBot.use(express.static('public'));
+myAppBot.listen(3000, function() {
+    console.log("Ecoute le port 3000");
+});
+
 
 //Avancer.
 
